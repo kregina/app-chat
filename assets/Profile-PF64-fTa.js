cae92ca492a287188bb8a39fd95666fc4ed2f1a1
kregina <kregina@users.noreply.github.com>
2024-02-09 13:47:49 +0000

    deploy: 08bdbe5e7cd3c64a7822e67e9d2903dff693a043

--- assets/Profile-PF64-fTa.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-6CeOe1Gc.js";function i(){return r.jsx("div",{children:r.jsx("h1",{children:"Profile"})})}export{i as default};
---
+++ assets/Profile-_W26GEAc.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-zgRgkQgS.js";function i(){return r.jsx("div",{children:r.jsx("h1",{children:"Profile"})})}export{i as default};
