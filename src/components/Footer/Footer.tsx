f750c17cad2bb8dc7f5ef58381dacfdbdf120441
Kelly Silva <kellynha756@gmail.com>
2024-02-12 17:36:23 +0200

    add emojipicker

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,44 +2,75 @@ import { useState } from 'react';
 
 import { Button } from '@components/Form/Button';
 import { Input } from '@components/Form/Input';
+import { useTheme } from '@components/Theme/useTheme';
+import data from '@emoji-mart/data';
+import Picker from '@emoji-mart/react';
 import { faFaceSmile, faPaperPlane } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import styles from './Footer.module.css';
 export const Footer = () => {
+  const { theme } = useTheme();
   const [newMessage, setNewMessage] = useState<string>('');
+  const [showEmojiPicker, setShowEmojiPicker] = useState<boolean>(false);
 
   const handleOnChange = (message: string) => {
     setNewMessage(message);
   };
 
-  const handleEmojiClick = () => {
-    console.log('emoji');
+  const sendMessage = () => {
+    console.log('Sending message:', newMessage);
+    setNewMessage('');
+  };
+
+  const toggleEmojiPicker = (event: { stopPropagation: () => void; }) => {
+    event.stopPropagation();
+    setShowEmojiPicker(!showEmojiPicker);
+  };
+
+  const addEmoji = (emoji: { native: string; }) => {
+    setNewMessage((currentMessage) => currentMessage + emoji.native);
   };
 
   return (
-
-    <footer className={styles.container} data-testid="footer">
-      <Button className="icon" id="emoji" onClick={handleEmojiClick}>
-        <FontAwesomeIcon
-          data-testid="button-icon"
-          icon={faFaceSmile}
-          className={styles.icon}
+    <>
+      {showEmojiPicker && (
+        <Picker
+          data={data}
+          onEmojiSelect={addEmoji}
+          className={styles.emojiPicker}
+          onClickOutside={() => setShowEmojiPicker(false)}
+          icons="outline"
+          previewEmoji="none"
+          theme={theme}
         />
-      </Button>
-      <Input
-        id="new-message"
-        onValueChange={handleOnChange}
-        placeholder="Type new message..."
-        value={newMessage}
-      />
-      <Button className="icon" id="send-message" onClick={handleEmojiClick}>
-        <FontAwesomeIcon
-          data-testid="button-icon"
-          icon={faPaperPlane}
-          className={styles.icon}
+      )}
+      <footer className={styles.container} data-testid="footer">
+        <Button
+          className="icon"
+          id="emoji"
+          onClick={toggleEmojiPicker}
+        >
+          <FontAwesomeIcon
+            data-testid="button-icon"
+            icon={faFaceSmile}
+            className={styles.icon}
+          />
+        </Button>
+        <Input
+          id="new-message"
+          onValueChange={(message) => handleOnChange(message)}
+          placeholder="Type new message..."
+          value={newMessage}
         />
-      </Button>
-    </footer>
+        <Button className="icon" id="send-message" onClick={sendMessage}>
+          <FontAwesomeIcon
+            data-testid="button-icon"
+            icon={faPaperPlane}
+            className={styles.icon}
+          />
+        </Button>
+      </footer>
+    </>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/app-chat/',
+  base: '/app-chat',
   plugins: [react()],
   resolve: {
     alias: {
