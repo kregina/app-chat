ff3c3e61bd73d5daa4172c555d8a2486cba3e18b
kregina <kregina@users.noreply.github.com>
2024-02-13 19:03:04 +0000

    deploy: b51dd02f5d48a241d38087f548b374daf1d0d716

---
+++ assets/Profile-0_u_qaAD.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-lg9bQGkK.js";function i(){return r.jsx("div",{children:r.jsx("h1",{children:"Profile"})})}export{i as default};
--- assets/Profile-DJexhDT7.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-xf226hu6.js";function i(){return r.jsx("div",{children:r.jsx("h1",{children:"Profile"})})}export{i as default};
