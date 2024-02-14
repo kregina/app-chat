16134d749f8793d9d0a78614b19a79efcf4788df
Kelly Silva <kellynha756@gmail.com>
2024-02-14 20:10:06 +0200

    fix typo on login page

--- src/pages/Home/Login/LoginHeader.tsx
+++ src/pages/Home/Login/LoginHeader.tsx
@@ -8,7 +8,7 @@ export const LoginHeader: FC = () => {
       <h4>Welcome to</h4>
       <h1 className={styles.h1}><strong>The Lobby&trade;</strong></h1>
       <p>
-        Step right up!
+        Step right up
         where the chat never stops and your next adventure is just a username away!
       </p>
     </>
